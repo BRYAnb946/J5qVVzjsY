48acdf21a70e00cf9509b1fd0cda957834efbd62
BRYAnb946 <yeasts.spruce.0j@icloud.com>
2025-10-11 14:26:40 +0100

    Enhance RUBY 负载测试工具

---
+++ load_tester_1011_2126_ihv.rb
@@ -0,0 +1,54 @@
+# 代码生成时间: 2025-10-11 21:26:39
+# load_tester.rb
+#
+# This Ruby script is a basic load testing tool designed to test the performance
+# of a service using the CUBA framework. It simulates multiple requests to
+# a specific endpoint and measures the response time.
+
+require 'httparty'
+require 'json'
+require 'logger'
+
+# Configuration for the load tester
+class LoadTesterConfig
+  attr_accessor :endpoint, :concurrency, :requests, :logger
+
+  def initialize(endpoint, concurrency, requests, logger_path = 'load_test.log')
+    @endpoint = endpoint
+    @concurrency = concurrency
+    @requests = requests
+    @logger = Logger.new(logger_path)
+  end
+end
+
+# LoadTester class responsible for the testing process
+class LoadTester
+  attr_reader :config
+  
+  def initialize(config)
+    @config = config
+  end
+
+  # Starts the load test
+  def start
+    puts 'Starting load test...'
+    @config.logger.info("Load test started with #{config.concurrency} concurrency and #{config.requests} requests")
+    
+    results = []
+    @config.requests.times do |i|
+      results << Thread.new { send_request(i) }
+    end
+    
+    results.each(&:join)
+    
+    puts 'Load test completed.'
+    @config.logger.info('Load test completed.')
+  end
+
+  # Sends a single request to the endpoint
+  def send_request(request_number)
+    response = HTTParty.get(config.endpoint)
+    
+    if response.success?
+      response_time = response.headers['X-Response-Time'].to_f
+      @config.logger.info(
